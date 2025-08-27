35a95ce92017b5b7b878e6b8c75c43a61d9cede1
Oleg Blednov <oleg.codev@gmail.com>
2025-08-27 16:40:25 +0300

    Implement test duration grouping by module, class, or function (#30)
    This PR adds support for grouping test durations by different criteria
    (module, class, function) as requested in issue #26.

--- src/pytest_durations/helpers.py
+++ src/pytest_durations/helpers.py
@@ -1,27 +1,105 @@
 """Internal helper functions module."""
-import contextlib
-from typing import TYPE_CHECKING
+from collections.abc import Mapping
+from contextlib import suppress
+from functools import partial
+from itertools import chain, groupby
+from operator import itemgetter
+from typing import TYPE_CHECKING, Callable, Literal
+
+from pytest_durations.types import GroupBy
 
 if TYPE_CHECKING:
     from _pytest.fixtures import FixtureDef
     from _pytest.nodes import Item
 
+    from pytest_durations.typing import DurationListT, FunctionKeyT, FunctionMeasurementsT
 
-def _get_fixture_key(fixturedef: "FixtureDef") -> str:
-    """Return fixture name."""
-    return fixturedef.argname
-
-
-def _get_test_key(item: "Item") -> str:
-    """Return test item name without filename part (class and function names only)."""
-    key = item.nodeid
-    with contextlib.suppress(IndexError):
-        # remove filename
-        key = key.split("::", 1)[1]
-    # remove parameters
-    return key.split("[", 1)[0]
+    MeasurementItemT = tuple[FunctionKeyT, DurationListT]
+    GroupingCbT = Callable[[MeasurementItemT], FunctionKeyT]
+    GroupingKindT = Literal["test", "fixture"]
 
 
-def _is_shared_fixture(fixturedef: "FixtureDef") -> bool:
+def is_shared_fixture(fixturedef: "FixtureDef") -> bool:
     """Return true if a fixture is shared."""
     return fixturedef.scope != "function"
+
+
+def get_fixture_key(fixturedef: "FixtureDef", item: "Item") -> "FunctionKeyT":
+    """Return fixture measurements dict key."""
+    baseid = fixturedef.baseid if fixturedef.baseid else get_test_key(item=item)
+    return "::".join(filter(None, (baseid, fixturedef.argname)))
+
+
+def get_test_key(item: "Item") -> "FunctionKeyT":
+    """Return test item measurements dict key."""
+    return item.nodeid.split("[", 1)[0]
+
+
+def _get_grouping_func(kind: "GroupingKindT", group_by: "GroupBy") -> "GroupingCbT":
+    """Get fixture key grouping function based on a GroupBy enumeration value."""
+    with suppress(KeyError):
+        return _GROUPING_FUNC_MAP[kind][group_by]
+    msg = f'{kind.capitalize()} grouping function for "{group_by}" not implemented'
+    raise NotImplementedError(msg)
+
+
+get_test_grouping_func = partial(_get_grouping_func, kind="test")
+get_fixture_grouping_func = partial(_get_grouping_func, kind="fixture")
+
+
+def get_grouped_measurements(
+    measurements: "FunctionMeasurementsT",
+    grouping_func: "GroupingCbT",
+) -> "FunctionMeasurementsT":
+    """Group test measurements using a provided function to get grouping keys."""
+    return {
+        k: list(chain(*map(itemgetter(1), v)))
+        for k, v in groupby(sorted(measurements.items(), key=grouping_func), key=grouping_func)
+    }
+
+
+def _test_group_by_legacy(item: "MeasurementItemT") -> "FunctionKeyT":
+    # keep class and test name only (old behaviour before grouping)
+    return item[0].split("::", 1)[-1]
+
+
+def _fixture_group_by_legacy(item: "MeasurementItemT") -> "FunctionKeyT":
+    # keep fixture name only (old behaviour before grouping)
+    return item[0].rsplit("::", 1)[-1]
+
+
+def _group_by_module(item: "MeasurementItemT") -> "FunctionKeyT":
+    # remove class and test name (keep file name)
+    with suppress(IndexError):
+        return item[0].split("::", 1)[-2]
+    return "uncertain"
+
+
+def _group_by_class(item: "MeasurementItemT") -> "FunctionKeyT":
+    # remove test name (keep file and class name)
+    with suppress(IndexError):
+        mod = item[0].split("::", 1)[-2]
+        cls = item[0].rsplit("::", 1)[-2]
+        return cls if cls != mod else f"{mod}::"
+    return f"uncertain::{item[0]}"
+
+
+def _group_by_function(item: "MeasurementItemT") -> "FunctionKeyT":
+    # keep name as is
+    return item[0]
+
+
+_GROUPING_FUNC_MAP: Mapping["GroupingKindT", Mapping["GroupBy", "GroupingCbT"]] = {
+    "test": {
+        GroupBy.LEGACY: _test_group_by_legacy,
+        GroupBy.MODULE: _group_by_module,
+        GroupBy.CLASS: _group_by_class,
+        GroupBy.FUNCTION: _group_by_function,
+    },
+    "fixture": {
+        GroupBy.LEGACY: _fixture_group_by_legacy,
+        GroupBy.MODULE: _group_by_module,
+        GroupBy.CLASS: _group_by_class,
+        GroupBy.FUNCTION: _group_by_function,
+    },
+}
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,24 +1,242 @@
-from unittest.mock import create_autospec
+from typing import TYPE_CHECKING, cast
 
 import pytest
-from _pytest.nodes import Item
 
-from pytest_durations.helpers import _get_test_key
-
-
-@pytest.mark.parametrize("param", [None, "param", "param1-param2"])
-@pytest.mark.parametrize(
-    ("nodeid", "expected"),
-    [
-        ("func", "func"),
-        ("filename.py::func", "func"),
-        ("filename.py::scope::func", "scope::func"),
-    ],
+from pytest_durations.helpers import (
+    _GROUPING_FUNC_MAP,
+    _get_grouping_func,
+    get_fixture_key,
+    get_grouped_measurements,
+    get_test_key,
+    is_shared_fixture,
 )
-def test_get_test_key(nodeid, param, expected):
-    """Filename and parameters should be removed from test key."""
-    if param:
-        nodeid = f"{nodeid}[{param}]"
-    item = create_autospec(Item, instance=True, nodeid=nodeid)
-    result = _get_test_key(item)
-    assert result == expected
+from pytest_durations.types import GroupBy
+
+if TYPE_CHECKING:
+    from _pytest.fixtures import FixtureRequest, SubRequest
+
+    from pytest_durations.helpers import MeasurementItemT
+    from pytest_durations.typing import FunctionKeyT, FunctionMeasurementsT
+
+
+@pytest.fixture
+def module_level():
+    ...
+
+
+class TestIsSharedFixture:
+    @pytest.fixture
+    def scoped(self):
+        ...
+
+    @pytest.fixture(scope="class")
+    def shared(self):
+        ...
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            (module_level.__name__, False),
+            (scoped.__name__, False),
+            (shared.__name__, True),
+        ],
+    )
+    @pytest.mark.usefixtures("module_level", "scoped", "shared")
+    def test_is_shared_fixture(self, request: "FixtureRequest", rule):
+        fixture, expected = rule
+        fixturedef = request._fixture_defs[fixture]
+        assert is_shared_fixture(fixturedef) is expected
+
+
+class TestGetFixtureKey:
+    @pytest.fixture(scope="class")
+    def class_level(self):
+        ...
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            (module_level.__name__, "tests/test_helpers.py::module_level"),
+            (class_level.__name__, "tests/test_helpers.py::TestGetFixtureKey::class_level"),
+            ("rule", "tests/test_helpers.py::TestGetFixtureKey::test_get_fixture_key::rule"),
+        ],
+    )
+    @pytest.mark.usefixtures("module_level", "class_level")
+    def test_get_fixture_key(self, request: "FixtureRequest",  rule):
+        fixture, expected = rule
+        fixturedef = request._fixture_defs[fixture]
+        result = get_fixture_key(fixturedef=fixturedef, item=request.node)
+        assert result == expected
+
+
+class TestGetTestKey:
+    def test_get_test_key(self, request: "FixtureRequest"):
+        result = get_test_key(item=request.node)
+        assert result == "tests/test_helpers.py::TestGetTestKey::test_get_test_key"
+
+    @pytest.mark.parametrize("param", [None])
+    def test_get_test_key_parametrized(self, request: "FixtureRequest", param):
+        result = get_test_key(item=request.node)
+        assert result == "tests/test_helpers.py::TestGetTestKey::test_get_test_key_parametrized"
+
+
+class TestGetGroupingFunc:
+    @pytest.fixture(params=[*GroupBy])
+    def group_by(self, request: "SubRequest"):
+        return request.param
+
+    @pytest.fixture(params=["test", "fixture"])
+    def kind(self, request: "SubRequest"):
+        return request.param
+
+    def test_get_grouping_func(self, group_by, kind):
+        result = _get_grouping_func(kind=kind, group_by=group_by)
+        assert callable(result) is True
+
+    def test_get_grouping_func_invalid(self):
+        with pytest.raises(NotImplementedError) as exc:
+            _get_grouping_func(kind="test", group_by=cast("GroupBy", "invalid"))
+        assert exc.match('Test grouping function for "invalid" not implemented')
+
+
+class TestGetGroupedMeasurements:
+    @pytest.fixture(scope="class")
+    def measurements(self) -> "FunctionMeasurementsT":
+        return {
+            "module.py::scope::function": [1.0],
+            "module.py::scope": [2.0],
+            "module.py": [3.0],
+        }
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            (1, {"module.py::scope::function": [1.0], "module.py::scope": [2.0], "module.py": [3.0]}),
+            (2, {"module.py::scope": [1.0, 2.0], "module.py": [3.0]}),
+            (3, {"module.py": [1.0, 2.0, 3.0]}),
+        ],
+    )
+    def test_get_grouped_measurements(self, measurements, rule):
+        def grouping_func(item: "MeasurementItemT") -> "FunctionKeyT":
+            return item[0].rsplit("::", depth)[0]
+
+        depth, expected = rule
+        get_grouped_measurements(measurements=measurements, grouping_func=grouping_func)
+
+
+class TestTestGroupBy:
+    @pytest.fixture
+    def assertion(self, rule):
+        def assertion(group_by: "GroupBy") -> bool:
+            (sample, expected), grouping_func = rule, _GROUPING_FUNC_MAP["test"][group_by]
+            result = grouping_func((sample, [1.0]))
+            return result == expected
+
+        return assertion
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function", "scope::function"),
+            ("module.py::function", "function"),
+            ("function", "function"),
+        ],
+    )
+    def test_group_by_legacy(self, assertion, rule):
+        assert assertion(group_by=GroupBy.LEGACY) is True
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function", "module.py"),
+            ("module.py::function", "module.py"),
+            ("function", "uncertain"),
+        ],
+    )
+    def test_group_by_module(self, assertion, rule):
+        assert assertion(group_by=GroupBy.MODULE) is True
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function", "module.py::scope"),
+            ("module.py::function", "module.py::"),
+            ("function", "uncertain::function"),
+        ],
+    )
+    def test_group_by_class(self, assertion, rule):
+        assert assertion(group_by=GroupBy.CLASS) is True
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function", "module.py::scope::function"),
+            ("module.py::function", "module.py::function"),
+            ("function", "function"),
+        ],
+    )
+    def test_group_by_function(self, assertion, rule):
+        assert assertion(group_by=GroupBy.FUNCTION) is True
+
+
+class TestFixtureGroupBy:
+    @pytest.fixture
+    def assertion(self, rule):
+        def assertion(group_by: "GroupBy") -> bool:
+            (sample, expected), grouping_func = rule, _GROUPING_FUNC_MAP["fixture"][group_by]
+            result = grouping_func((sample, []))
+            return result == expected
+
+        return assertion
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function::fixture", "fixture"),
+            ("module.py::scope::fixture", "fixture"),
+            ("module.py::function::fixture", "fixture"),
+            ("module.py::fixture", "fixture"),
+            ("fixture", "fixture"),
+        ],
+    )
+    def test_group_by_legacy(self, assertion, rule):
+        assert assertion(group_by=GroupBy.LEGACY) is True
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function::fixture", "module.py"),
+            ("module.py::scope::fixture", "module.py"),
+            ("module.py::function::fixture", "module.py"),
+            ("module.py::fixture", "module.py"),
+            ("fixture", "uncertain"),
+        ],
+    )
+    def test_group_by_module(self, assertion, rule):
+        assert assertion(group_by=GroupBy.MODULE) is True
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function::fixture", "module.py::scope::function"),
+            ("module.py::scope::fixture", "module.py::scope"),
+            ("module.py::function::fixture", "module.py::function"),
+            ("module.py::fixture", "module.py::"),
+            ("fixture", "uncertain::fixture"),
+        ],
+    )
+    def test_group_by_class(self, assertion, rule):
+        assert assertion(group_by=GroupBy.CLASS) is True
+
+    @pytest.mark.parametrize(
+        "rule",
+        [
+            ("module.py::scope::function::fixture", "module.py::scope::function::fixture"),
+            ("module.py::scope::fixture", "module.py::scope::fixture"),
+            ("module.py::function::fixture", "module.py::function::fixture"),
+            ("module.py::fixture", "module.py::fixture"),
+            ("fixture", "fixture"),
+        ],
+    )
+    def test_group_by_function(self, assertion, rule):
+        assert assertion(group_by=GroupBy.FUNCTION) is True
